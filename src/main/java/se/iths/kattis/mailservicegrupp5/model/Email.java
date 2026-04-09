0a531f1e414c1c8bb04de916fff4f6706d7a75fd
Kattis82 <kattiscalmvik@outlook.com>
2026-04-09 19:59:09 +0200

    Lagt till konstruktor

--- src/main/java/se/iths/kattis/mailservicegrupp5/model/Email.java
+++ src/main/java/se/iths/kattis/mailservicegrupp5/model/Email.java
@@ -1,10 +1,12 @@
 package se.iths.kattis.mailservicegrupp5.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Email {
 
     private String recipient;
